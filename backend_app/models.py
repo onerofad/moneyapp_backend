65560c4fa96803d65555b010aca6403bfbf5746f
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-01-26 20:04:31 +0100

    first commit

--- backend_app/models.py
+++ backend_app/models.py
@@ -6,6 +6,7 @@ class Register(models.Model):
     phone = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
+    verify = models.IntegerField(default=0)
 
     def __str__(self):
         return self.fname
