9c9a83ae8f7b63ae6eb05a5eb3a4a5362206dca1
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-01-27 21:20:46 +0100

    first commit

--- backend_app/models.py
+++ backend_app/models.py
@@ -6,6 +6,7 @@ class Register(models.Model):
     phone = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     address = models.TextField(default = '')
+    dob = models.DateField(default='2024-01-24')
     password = models.CharField(max_length=255)
     verify = models.IntegerField(default=0)
 
