6cf1598a960ed70fbcf8e50529fc5e8aee7d1b2d
frank@gmail.com <imafidonfrank2015@gmail.com>
2024-05-08 19:32:48 +0100

    first commit

--- backend_app/admin.py
+++ backend_app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Register, ExchangeRates, Transactions, PaymentMethod, BankInfo
+from .models import Register, ExchangeRates, Transactions, PaymentMethod, BankInfo, Recepients
 
-admin.site.register({Register, ExchangeRates, Transactions, PaymentMethod, BankInfo})
+admin.site.register({Register, ExchangeRates, Transactions, PaymentMethod, BankInfo, Recepients})
